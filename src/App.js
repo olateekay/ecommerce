703d96a54f6a0c2868d06971b9e1a9ba86e5bc1b
olateekay <olatokunboalege@gmail.com>
2020-06-03 09:24:19 +0100

    App Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Switch,Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './Pages/HomePage/homePage';
 
+const HatsPage = () => (
+  <div>HATS PAGE</div>
+);
+
 function App() {
   return (
     <div>
-    <HomePage/>
+      <Switch>
+    <Route exact path='/' component={HomePage} />
+    <Route path='/hats' component={HatsPage} />
+    </Switch>
     </div>
   );
 }
--- src/Components/Directory/directory.jsx
+++ src/Components/Directory/directory.jsx
@@ -12,7 +12,7 @@ class Directory extends React.Component {
                     title: 'Hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                   },
                   {
                     title: 'Jackets',
@@ -45,9 +45,15 @@ class Directory extends React.Component {
     }
     render () {
         return (
+            // <div className='directory-menu'>
+            //     {this.state.sections.map(({title,imageUrl,id,size,linkUrl})  => (
+            //         <MenuItem key={id} title= {title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
+            //     ))}
+            // </div>
+            // or rewrite using the spread operator Es6
             <div className='directory-menu'>
-                {this.state.sections.map(({title,imageUrl,id,size})  => (
-                    <MenuItem key={id} title= {title} imageUrl={imageUrl} size={size}/>
+                {this.state.sections.map(({ id, ...otherSectionProps})  => (
+                    <MenuItem key={id} {...otherSectionProps}/>
                 ))}
             </div>
         )
